42d68bf655c19bbe237350954f18291502daba69
tinbrletic <tin.pingvin@gmail.com>
2024-09-17 20:41:44 +0200

    section 2

---
+++ 01-Fundamentals-Part-1/starter/homework.js
@@ -0,0 +1,127 @@
+// //Values and Variables
+
+// let population = 4;
+
+// // console.log(country);
+// // console.log(continent);
+// // console.log(population);
+
+// // Data Types
+
+// let language;
+
+// // console.log(isIsland);
+// // console.log(population);
+// // console.log(country);
+// // console.log(language);
+
+// // let, const and var
+
+// language = "croatian";
+// // language = "english";
+
+// const country = "Croatia";
+// const continent = "Europe";
+// const isIsland = false;
+
+// //isIsland = true;
+
+// // Basic Operators
+
+// // console.log(population / 2);
+
+// // console.log(++population);
+
+// // console.log(population > 6);
+// // console.log(population < 33);
+
+// const description1 =
+//   country +
+//   " is in " +
+//   continent +
+//   ", and its " +
+//   population +
+//   " million people speak " +
+//   language;
+
+// // console.log(description);
+
+// // Strings and Template Literals
+
+// const description2 = `${country} is in ${continent}, and it's ${population} million people speak ${language}`;
+
+// // console.log(description2);
+
+// // Taking Decisions: if / else Statements
+
+// // population = 130;
+
+// // if (population < 33) {
+// //   console.log(
+// //     `${country}'s population is ${33 - population} million below average`
+// //   );
+// // } else {
+// //   console.log(`${country}'s population is above average`);
+// // }
+
+// //// Type Conversion And Coercion
+
+// // console.log("9" - "5"); // -> 4
+// // console.log("19" - "13" + "17"); // -> "617"
+// // console.log("19" - "13" + 17); // -> 23
+// // console.log("123" < 57); // -> false
+// // console.log(5 + 6 + "4" + 9 - 4 - 2); // -> 1143
+
+// //// Equality Operators: == vs. ===
+
+// // const numNeighbours = Number(
+// //   prompt(`How many neighbour countries does your contry have?`)
+// // );
+
+// // if (numNeighbours === 1) {
+// //   console.log(`Only 1 border!`);
+// // } else if (numNeighbours > 1) {
+// //   console.log(`More than 1 border`);
+// // } else {
+// //   console.log(`No borders`);
+// // }
+
+// //// Logical Operators
+
+// // if (language === `english` && population < 50 && !isIsland) {
+// //   console.log(`You should live in ${country} :)`);
+// // } else {
+// //   console.log(`${country} does not meet your criteria :(`);
+// // }
+
+// //// The switch Statement
+
+// // language = "croatian";
+
+// // switch (language) {
+// //   case "chinese":
+// //   case "mandarin":
+// //     console.log(`MOST number of native speakers!`);
+// //     break;
+// //   case "spanish":
+// //     console.log(`2nd place in number of native speakers`);
+// //     break;
+// //   case "english":
+// //     console.log(`3rd place`);
+// //     break;
+// //   case "hindi":
+// //     console.log(`Number 4`);
+// //     break;
+// //   case "arabic":
+// //     console.log(`5th most spoken language`);
+// //     break;
+// //   default:
+// //     console.log(`Great language too :D`);
+// //     break;
+// // }
+
+// //// The Conditional (Ternary) Operator
+
+// // console.log(
+// //   `${country}'s population is ${population > 33 ? "above" : "below"} average`
+// // );
