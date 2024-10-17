898c563441357457c2fb7981fcdff823fc34c449
tinbrletic <tin.pingvin@gmail.com>
2024-10-17 20:57:29 +0200

    section 18 - dodatak na prvi dio

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,15 +1,13 @@
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     // console.log(res, data);
     const { recipe } = data.data;
@@ -25,7 +23,7 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    console.log(err);
-    alert(err);
+    // temp error handling
+    console.log(`${err} 💥💥💥💥💥`);
   }
 };
